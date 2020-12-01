38921c3f97f569db3a61682281f177401a327941
kye <kye135@github.com>
2020-12-01 23:27:13 +0900

    예약페이지 > - 예약등록 중 취소시 스크립트 수정 - datepicker와 fullcalendar 날짜 연동 - week, month 선택한 날짜로 목록 출력 오류 수정

--- src/main/resources/static/assets/js/common.js
+++ src/main/resources/static/assets/js/common.js
@@ -92,3 +92,12 @@ function uploadSummernoteImageFile(file, editor) {
         }
     });
 }
+
+function getFormatDate(date){
+    var year = date.getFullYear();
+    var month = (1 + date.getMonth());
+    month = month >= 10 ? month : '0' + month;
+    var day = date.getDate();
+    day = day >= 10 ? day : '0' + day;
+    return year + '-' + month + '-' + day;
+}
